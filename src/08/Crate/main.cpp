a01f2f74362edfa563ee174694dd23f20db735b9
Vasilios Magriplis <etameson@gmail.com>
2026-07-31 10:25:45 +1000

    Begin work on Crate

---
+++ src/08/Crate/main.cpp
@@ -0,0 +1,19 @@
+import std;
+import shared;
+//import litskull;
+
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
+auto wWinMain(Win32::HINSTANCE hInstance, Win32::HINSTANCE, Win32::LPWSTR, int) -> int
+try
+{
+	if constexpr (IsDebugBuild)
+		Win32::_CrtSetDbgFlag(Win32::CrtAllocMemDf | Win32::CrtLeakCheckDf);
+	return 0;// LitSkullApp{ hInstance }.Run();
+}
+catch (const std::exception& e)
+{
+	ErrorMsg(e);
+	return 1;
+}
