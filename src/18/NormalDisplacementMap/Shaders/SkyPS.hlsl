f384b8848e260216b692dd239723ec2bb11eaee8
Vasilios Magriplis <etameson@gmail.com>
2026-08-24 20:41:50 +1000

    Begin work on next project.

---
+++ src/18/NormalDisplacementMap/Shaders/SkyPS.hlsl
@@ -0,0 +1,17 @@
+//=============================================================================
+// Sky.fx by Frank Luna (C) 2011 All Rights Reserved.
+//
+// Effect used to shade sky dome.
+//=============================================================================
+
+#include "SkyCommon.hlsli"
+
+// Nonnumeric values cannot be added to a cbuffer.
+TextureCube gCubeMap : register(t0);
+
+SamplerState samTriLinearSam : register(s0);
+
+float4 main(VertexOut pin) : SV_Target
+{
+    return gCubeMap.Sample(samTriLinearSam, pin.PosL);
+}
