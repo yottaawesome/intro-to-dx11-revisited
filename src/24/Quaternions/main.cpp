8d3803683b28629d5e2d0ea4e5286f01aef30fce
Vasilios Magriplis <etameson@gmail.com>
2026-08-31 20:19:17 +1000

    Begin work on the final chapters.

---
+++ src/24/Quaternions/main.cpp
@@ -0,0 +1,17 @@
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
+import std;
+import shared;
+
+auto wWinMain(Win32::HINSTANCE hInstance, Win32::HINSTANCE, Win32::LPWSTR, int) -> int
+try
+{
+	Win32::SetDebugBuildFlag(Win32::CrtAllocMemDf | Win32::CrtLeakCheckDf);
+	return 0;
+}
+catch (const std::exception& e)
+{
+	ErrorMsg(e);
+	return 1;
+}
